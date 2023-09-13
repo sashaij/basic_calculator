d6db513570eaca386c15431bc18e3c0ae0437d96
sashaij <o.zagorodniuk@outlook.com>
2023-09-13 18:43:23 +0300

    add functionality for all the operations

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 'use strict'
 
+let numberOne = 36;
+let numberTwo = 6;
+let operator = '*';
+
 const add = function(a, b) {
 	return a + b;
 };
@@ -21,8 +25,19 @@ const multiply = function(num1, ...nums) {
     numbs.forEach(num => {
     divi /= num;
   })
-  console.log(divi);
   return divi;
   }
 
-  divide(8, 2);
+  const operate = function (numbOne, numbTwo, oPerat) {
+    if(oPerat === '+') {
+        console.log(add(numbOne, numbTwo));
+    } else if (oPerat === '-') {
+        console.log(subtract(numbOne, numbTwo));
+    } else if (oPerat === '*') {
+        console.log(multiply(numbOne, numbTwo));
+    } else if (oPerat === '/') {
+        console.log(divide(numbOne, numbTwo));
+    }
+  }
+
+  operate(numberOne, numberTwo, operator);
